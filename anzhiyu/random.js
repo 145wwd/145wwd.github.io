19f088f78435e0c06d723d83252d5305e2a86de8
145wwd <zhengyichuan234@gmail.com>
2025-07-10 22:09:12 +0800

    Site updated: 2025-07-10 22:09:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/10/盲派八字/","2025/07/10/怎么看八字/"];function toRandomPost(){
+var posts=["2025/07/10/盲派八字/","2025/07/10/盲派命理中天干与地支的作用关系解析/","2025/07/10/怎么看八字/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
