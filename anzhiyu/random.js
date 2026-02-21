6c8d755535688606af252adb652a8977168ee7ea
145wwd <zhengyichuan234@gmail.com>
2026-02-21 17:29:37 +0800

    Site updated: 2026-02-21 17:29:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/21/大六壬的妙用/"];function toRandomPost(){
+var posts=["2026/02/21/大六壬的妙用/","2026/02/21/盲派八字命理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
